f84ba3c96dac49d5c7b48b3ff2111ed8486f34a8
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-06-21 22:50:12 -0300

    Creating route list all task

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { CreateTaskController } from './controllers/task/CreateTaskController';
+import { ListTaskController } from './controllers/task/ListTaskController';
 
 const router = Router();
 
 router.post('/task', new CreateTaskController().handle);
+router.get('/task', new ListTaskController().handle);
 
 export { router };
