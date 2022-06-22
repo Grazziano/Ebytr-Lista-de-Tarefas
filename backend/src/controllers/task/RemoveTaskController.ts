bdf06f43d30d17ef347953c4b90decbb45bd3c4f
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-06-21 23:33:28 -0300

    Remove task controller

---
+++ backend/src/controllers/task/RemoveTaskController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import { RemoveTaskService } from '../../services/task/RemoveTaskService';
+
+class RemoveTaskController {
+  async handle(req: Request, res: Response) {
+    const task_id = req.query.task_id as string;
+
+    const removeTask = new RemoveTaskService();
+
+    const result = await removeTask.execute({ task_id });
+
+    return res.status(200).end();
+  }
+}
+
+export { RemoveTaskController };
