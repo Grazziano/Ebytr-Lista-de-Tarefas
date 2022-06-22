2119ae56b551c8d5435371194e5d1f6453f09271
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-06-21 23:33:45 -0300

    Endpoint to remove task

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { CreateTaskController } from './controllers/task/CreateTaskController';
 import { ListTaskController } from './controllers/task/ListTaskController';
+import { RemoveTaskController } from './controllers/task/RemoveTaskController';
 
 const router = Router();
 
 router.post('/task', new CreateTaskController().handle);
 router.get('/task', new ListTaskController().handle);
+router.delete('/task/remove', new RemoveTaskController().handle);
 
 export { router };
