4ebabae1766d4298b2adc89e3accf57c55ed0609
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-07-01 23:44:16 -0300

    Back orm to mysql database

--- backend/prisma/migrations/20220702020613_create_task_list/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "tasks" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "status" TEXT NOT NULL DEFAULT 'pendente',
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
